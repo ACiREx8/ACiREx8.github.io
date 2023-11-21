2e917a7fce0e8478d57b80f1f521e606f9a56cf8
ACiREx8 <115101824+ACiREx8@users.noreply.github.com>
2023-11-20 21:45:52 -0500

    Let's try this

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./miscellaneous.html", "./resume.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
